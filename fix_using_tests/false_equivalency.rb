2a202605912aa9ad60b5414133af8fff917db3bb
Young Han <han20@umbc.edu>
2019-10-25 22:04:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,5 @@
 
-
+require 'pry'
 def get_user_input
   gets.chomp
 end
@@ -16,6 +16,7 @@ def selection(num)
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
   end
+      binding.pry
 end
 
 def runner
