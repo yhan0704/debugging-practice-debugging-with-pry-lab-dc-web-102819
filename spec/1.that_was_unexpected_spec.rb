29e02c385aefcd8c1aa38e6bd3cd0d0e50e88137
Young Han <han20@umbc.edu>
2019-10-25 22:08:06 +0000

    Automatically backed up by Learn

--- spec/1.that_was_unexpected_spec.rb
+++ spec/1.that_was_unexpected_spec.rb
@@ -1,9 +1,7 @@
 require_relative '../first_priority/that_was_unexpected'
-require 'pry'
 describe "that_was_unexpected" do
   let(:baller) {Person.new('Jordan', true)}
   it "should tell me if the Person is a ballin candy lover" do
     expect(baller.ballin_candy_lover?).to eq("Aw sweet, this cat loves them some candy!")
-    binding.pry
   end
 end
