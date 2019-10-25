49bd401d3516155675bb6e93999069a290199127
Young Han <han20@umbc.edu>
2019-10-25 22:07:44 +0000

    Automatically backed up by Learn

--- spec/1.that_was_unexpected_spec.rb
+++ spec/1.that_was_unexpected_spec.rb
@@ -4,5 +4,6 @@ describe "that_was_unexpected" do
   let(:baller) {Person.new('Jordan', true)}
   it "should tell me if the Person is a ballin candy lover" do
     expect(baller.ballin_candy_lover?).to eq("Aw sweet, this cat loves them some candy!")
+    binding.pry
   end
 end
