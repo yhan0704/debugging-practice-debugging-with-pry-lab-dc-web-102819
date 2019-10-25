fa19e3ac5b05fbe58d17277cc66980f6388a364c
Young Han <han20@umbc.edu>
2019-10-25 22:15:10 +0000

    Automatically backed up by Learn

--- spec/classical_gas_spec.rb
+++ spec/classical_gas_spec.rb
@@ -4,5 +4,6 @@ describe "classical_gas" do
   let(:dinobones) {GasStation.new("Dino Bones Gas and Grill", 60)}
   it "is able to change the price of a gas station" do
     expect(dinobones.unleaded_price = 100).to eq(100)
+    binding.pry
   end
 end
