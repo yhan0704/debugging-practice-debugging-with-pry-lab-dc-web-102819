c9c8e78f6531107cc40f936adeb92665a38709cc
Young Han <han20@umbc.edu>
2019-10-25 22:27:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -1,7 +1,9 @@
 require 'pry'
 class GasStation
 
-  attr_reader :brand, :unleaded_price
+  attr_reader :brand, 
+   attr_accessor :unleaded_price
+
   
   def initialize(brand, unleaded_price)
     @brand = brand
