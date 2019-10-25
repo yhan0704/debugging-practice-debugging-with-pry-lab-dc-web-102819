0a05487e0e299c2906a948840bf94020089ae5b5
Young Han <han20@umbc.edu>
2019-10-25 22:19:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -5,12 +5,12 @@ class GasStation
 
   def initialize(brand, unleaded_price)
     @brand = brand
-    @unleaded_price = unleaded_price+40
+    @unleaded_price = unleaded_price
   end
 
 end
 
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
-dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
+dinobones = GasStation.new("Dino Bones Gas and Grill", 100)
 binding.pry
