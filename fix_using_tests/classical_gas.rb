8259b805ba924be46ab3db7ede11506811afbd49
Young Han <han20@umbc.edu>
2019-10-25 22:18:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -12,3 +12,4 @@ end
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
+binding.pry
